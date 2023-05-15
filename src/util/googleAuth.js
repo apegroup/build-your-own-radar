cc4510c2cf9aa7dc863614a245dc1801751ea230
markwtierney <markwtierney@users.noreply.github.com>
2023-05-15 12:56:15 +0200

    Update googleAuth.js

--- src/util/googleAuth.js
+++ src/util/googleAuth.js
@@ -2,8 +2,10 @@
 const d3 = require('d3')
 
 // Client ID and API key from the Developer Console
-var CLIENT_ID = process.env.CLIENT_ID
-var API_KEY = process.env.API_KEY
+// var CLIENT_ID = process.env.CLIENT_ID
+// var API_KEY = process.env.API_KEY
+var CLIENT_ID = '265967433756-8pf4e1t45r9q4nkn083csoq46lffvvta.apps.googleusercontent.com'
+var API_KEY = 'AIzaSyBhRSUsZDWQ_eI8mzFlycfiPccbrBUcwwM'
 
 // Array of API discovery doc URLs for APIs used by the quickstart
 var DISCOVERY_DOCS = ['https://sheets.googleapis.com/$discovery/rest?version=v4']
